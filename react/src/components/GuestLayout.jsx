06f06023e307a7564746364bbb928f90e068af6a
nemanjamartic <nemanja.martic@gmail.com>
2025-07-02 19:44:18 +0200

    laravel react project init push

---
+++ react/src/components/GuestLayout.jsx
@@ -0,0 +1,12 @@
+import {Outlet} from 'react-router-dom'
+
+export default function GuestLayout() {
+    return (
+        <div>
+            <div>
+                For guest users only
+                <Outlet />
+            </div>
+        </div>
+    );
+}
---
+++ react/src/router.jsx
@@ -0,0 +1,49 @@
+import {createBrowserRouter} from 'react-router-dom';
+import DefaultLayout from './components/DefaultLayout';
+import GuestLayout from './components/GuestLayout';
+import Dashboard from './views/Dashboard';
+import Login from './views/Login';
+import NotFound from './views/NotFound';
+import Register from './views/Register';
+import Users from './views/Users';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Users />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/register',
+                element: <Register />
+            },
+        ]
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    }
+]);
+
+export default router;
